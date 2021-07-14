51e16abb1db1ee1e1d50bede2f7ee380105c1021
Mark <mark@example.dev>
2021-07-14 16:05:36 -0700

    error handling, styling

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 const key = 'dl3Nguz5OZ7Pgmijm5sJcE6ow0PAx0R0';
 console.log("Let's get this party started!");
 
-async function searchGifs(key, searchTerm){
+async function searchGifs(searchTerm){
     const queries = {'api_key': key, 'q': searchTerm};
-    const response = await axios.get('https://api.giphy.com/v1/gifs/search', {
-        params: queries,
-        timeout: 10000
-    });
-    console.log(response.data);
+    let response;
+    // Send the request to Giphy API
+    try{
+        response = await axios.get('https://api.giphy.com/v1/gifs/search', {
+            params: queries,
+            timeout: 10000
+        });
+        console.log(response.data);
+    } catch (err) {
+        alert('Sorry! Cannot reach the Giphy API at this time. Error Message:' + err.message);
+    }
+
+    // Handle no search results
+    if (response.data.data.length === 0){
+        alert('The search yielded no results. Try a different term or leave the field empty to get a random GIF.')
+        return;
+    }
+    // Append to DOM
     const randomGif = randomChoice(response.data.data);
     appendGif(randomGif.images['downsized_medium'].url);
 }
 
+async function randomGif(){
+    const queries = {'api_key': key};
+    //Send the rquest to Giphy API
+    try {
+        const response = await axios.get('https://api.giphy.com/v1/gifs/random', {
+            params: queries,
+            timeout: 10000
+        });
+        console.log(response);
+        appendGif(response.data.data.images['downsized_medium'].url);
+    } catch (err) {
+        alert('Sorry! Cannot reach the Giphy API at this time. Error Message:' + err.message);
+    }
+
+}
+
 function appendGif(url){
     $('<img>')
-    .attr('src', url)
+    .attr({src: url, alt: 'Could not load image'})
     .appendTo($('#results'));
 }
 
@@ -23,11 +52,21 @@ function randomChoice(array){
 }
 
 $('form').on('submit', function(e){
-    e.preventDefault();
+    e.preventDefault();;
     const searchTerm = this.searchField.value;
-    searchGifs(key, searchTerm);
+    
+    // Giphy API requires a non-empty string to query the search endpoint
+    if (searchTerm){
+        searchGifs(searchTerm);
+    } else {
+        randomGif();
+    }
+    
     this.searchField.value = '';
 });
 
-
-//response.data[index].images['downsized medium'].url
+$('#delete').click(function(e){
+    e.preventDefault();
+    console.log('removed');
+    $('#results').empty();
+})
